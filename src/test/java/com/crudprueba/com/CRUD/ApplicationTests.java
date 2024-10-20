371544287d8ab52929eba103e5fe8b20ea3d855e
ALEXSTR19 <alex129t@outlook.com>
2024-10-20 02:42:23 -0500

    logro: stage all 1

---
+++ src/test/java/com/crudprueba/com/CRUD/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudprueba.com.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
